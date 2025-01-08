6b67fbad48031d41dc8cab1ffd8df51321128200
Prince140903 <aadityarevandkar@gmail.com>
2025-01-09 01:36:53 +0530

    temp

--- DashboardRepo/src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Header } from "./Header/header";
-export { default as Footer } from "./Footer/footer";
-export { default as Sidebar } from "./Sidebar/sidebar";
-export { default as UserImg } from "./UserImg/userImg";
-export { default as Box } from "./Box/box";
-
--- DashboardRepo/src/constants/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Images } from "./images.js";
-export { default as DynamicIcon } from "./icons.jsx";
